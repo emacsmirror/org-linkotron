6cbe806e92fd3602e6588a8f540154f48a52fea1
Per Weijnitz <per.weijnitz@gmail.com>
2020-01-10 17:13:27 +0100

    Replace implementation with a nice contribution from alphapapa.

--- org-linkotron.el
+++ org-linkotron.el
@@ -21,9 +21,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;
-;;
-;;
 ;;; Commentary:
 ;;
 ;; The purpose of this package is to provide a way to open a group of
@@ -63,11 +60,13 @@
 ;; invoke
 ;; org-linkotron-open-group
 ;;
+;;; Credits:
 ;;
-;;; Notes:
-;;
-;; Much of the project scaffolding was borrowed from alphapapa's org-super-agenda.
-;;
+;;  - Thanks to [[https://github.com/alphapapa][alphapapa]] for supplying an
+;;    elegant implementation and great feedback.
+;;    I also borrowed much of the project structure of his
+;;    org-super-agenda project, as well as the test script
+;;    [[https://github.com/alphapapa/makem.sh][makem.sh]].
 ;;
 ;;; License:
 ;;
@@ -90,92 +89,40 @@
 ;;; Code:
 
 (require 'org)
+(require 'ol)
 
 (defgroup org-linkotron nil "Org link batch opener"
   :prefix "org-linkotron-"
   :group 'convenience)
 
-(defcustom org-linkotron-pause 1.0 "Sleep between each link opening.
+(defcustom org-linkotron-pause 0.1 "Sleep between each link opening.
 Especially for web links, it seems nice to give
 Firefox some time between each call, especially for slower
 computers.  Specify the number of seconds to sleep here."
   :type '(float)
   :group 'org-linkotron)
 
-;; Borrowed most of this from https://orgmode.org/worg/org-hacks.html
-(defun org-linkotron--back-to-heading ()
-  "Go back to the current heading."
-  (interactive)
-  (forward-char) ; to get a match on the same line, if on a header
-  (cond
-   ((re-search-backward "^\*" nil t) (forward-line 1))
-   (t (goto-char (point-min)))))
-
-;; Borrowed most of this from https://orgmode.org/worg/org-hacks.html
-(defun org-linkotron--forward-to-heading ()
-  "Go forward to the next heading."
-  (interactive)
-  (cond
-   ((re-search-forward "^\*" nil t) (end-of-line 0))
-   (t (goto-char (point-max)))))
-
-(defun org-linkotron--find-region ()
-  "Return a region to search for links.
-The region is returned as a list of the start point and the end point."
-  (save-excursion
-    (org-linkotron--back-to-heading)
-    (let ((start (point)))
-      (org-linkotron--forward-to-heading)
-      (list start (point)))))
-
-(defun org-linkotron--next-link ()
-  "Search for the next org-link.
-Returns nil if none is found."
-  (if (re-search-forward "\\(\\[\\[.*?\\]\\[.*?\\]\\]\\)" nil t)
-      (match-beginning 1)))
-
-(defun org-linkotron--get-link-on-line (&rest may-step-line)
-  "Return the nearest link following the current position.
-If MAY-STEP-LINE is t, then allow search on the following line."
-  (save-restriction
-    (narrow-to-region (point) (line-end-position))
-    (let ((link-pos (org-linkotron--next-link)))
-      (or link-pos
-          (if may-step-line
-              (progn
-                (widen)
-                (forward-line);; FIXME: add error check
-                (beginning-of-line)
-                (org-linkotron--get-link-on-line)))))))
-
-(defun org-linkotron--get-group ()
-  "Return a list of points to the org-links found in the current (sub) heading region."
-  (save-excursion
-    (let ((region (org-linkotron--find-region)))
-      (save-restriction
-        (narrow-to-region (car region) (nth 1 region))
-        (goto-char (car region))
-        (let ((result '())
-              (link (org-linkotron--get-link-on-line)))
-          (while link
-            (setq result (nconc result (list link)))
-            (setq link (org-linkotron--get-link-on-line t)))
-          result)))))
+(defun org-linkotron--get-positions ()
+  "Get positions of the org-lins found in the current (sub) heading region."
+  (let ((bound (org-entry-end-position)))
+    (save-excursion
+      (goto-char (org-entry-beginning-position))
+      (cl-loop while (re-search-forward org-link-bracket-re bound t)
+               collect (match-beginning 0)))))
 
 ;;;###autoload
 (defun org-linkotron-open-group ()
   "Open the org-links found in the current (sub) heading region."
   (interactive)
   (save-excursion
-    (let* ((group (org-linkotron--get-group))
+    (let* ((positions (org-linkotron--get-positions))
            (progress-reporter
             (make-progress-reporter "Opening links...")))
-      (mapc (lambda (link)
-              (progress-reporter-update progress-reporter)
-              (goto-char link)
-              (org-open-at-point)
-              (sit-for org-linkotron-pause))
-            group)
+      (dolist (pos positions)
+        (progress-reporter-update progress-reporter)
+        (goto-char pos)
+        (org-open-at-point)
+        (sit-for org-linkotron-pause))
       (progress-reporter-done progress-reporter))))
 
 (provide 'org-linkotron)
