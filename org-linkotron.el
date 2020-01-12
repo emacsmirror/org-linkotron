d0adc5247b205bc73d2f1a83d4a512d2be541eb5
Per Weijnitz <per.weijnitz@gmail.com>
2020-01-12 23:35:28 +0100

    Bump version (messed up the order tagging and bumbing for 0.9.2).

--- org-linkotron.el
+++ org-linkotron.el
@@ -3,7 +3,7 @@
 ;; Author: Per Weijnitz <per.weijnitz@gmail.com>
 ;; Keywords: hypermedia, Org
 ;; URL: https://gitlab.com/perweij/org-linkotron
-;; Version: 0.9.2
+;; Version: 0.9.3
 ;; Package-Requires: ((emacs "26.1") (org "9.3"))
 
 ;; Copyright (C) 2019, Per Weijnitz, all rights reserved.
